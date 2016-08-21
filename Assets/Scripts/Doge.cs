fac5eba5102a8aa6bc92e54109fdb74f77c59026
dannew1 <daniel.willforss@skola.ljungby.se>
2016-08-21 14:04:39 +0200

    first commit

---
+++ Assets/Scripts/Doge.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doge : MonoBehaviour
+{
+    private float time_in_air = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_doge();
+        gravity();
+    }
+
+    public void move_doge()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position -= new Vector3(0, -4, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, 1, 0);
+        }
+    }
+
+    public void gravity()
+    {
+
+        if (transform.position.y <= 100)
+        {
+            transform.position = new Vector3(transform.position.x, 100, 0);
+            time_in_air = 0;
+        }
+
+        else
+        {
+            time_in_air += Time.deltaTime;
+            transform.position -= new Vector3(0, 1, 0) * 3 * time_in_air;
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject other_obj = collider.gameObject;
+
+        if (other_obj.GetComponent<Enemy>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Who_are_we.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Who_are_we : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("hej");
+    }
+}
