5b4b9ec21d561295c09809900502c68b5010786d
dannew1 <daniel.willforss@skola.ljungby.se>
2016-10-09 11:40:45 +0200

    spring och hopp fysik

--- Assets/Scripts/Doge.cs
+++ Assets/Scripts/Doge.cs
@@ -4,39 +4,72 @@ using System.Collections;
 public class Doge : MonoBehaviour
 {
     private float time_in_air = 0;
-
+    private Rigidbody2D rigid;
+    private bool dogeIsGrounded = false;
     // Use this for initialization
     void Start()
     {
-
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(10, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         move_doge();
         //gravity();
     }
 
     public void move_doge()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        
+            if (Input.GetKey(KeyCode.LeftArrow) && rigid.velocity.x > 0)
         {
-            transform.position -= new Vector3(1, 0, 0);
+            rigid.velocity += new Vector2(-2, 0);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if(Input.GetKey(KeyCode.LeftArrow) && nonTopSpeed())
         {
-            transform.position -= new Vector3(-1, 0, 0);
+            rigid.velocity += new Vector2(-1, 0);
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.RightArrow) && rigid.velocity.x < 0)
         {
-            transform.position -= new Vector3(0, -1, 0);
+            rigid.velocity += new Vector2(2, 0);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) && nonTopSpeed())
         {
-            transform.position -= new Vector3(0, 1, 0);
+            rigid.velocity += new Vector2(1, 0);
         }
+
+        if (Input.GetKey(KeyCode.UpArrow) && noYMovement())
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 100);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigid.velocity += new Vector2(0, -1);
+        }
+    }
+
+    private bool noYMovement()
+    {
+        return rigid.velocity.y <= 10 && rigid.velocity.y >= -10 && dogeIsGrounded;
+    }
+
+    private bool nonTopSpeed()
+    {
+        return rigid.velocity.x <= 100 && rigid.velocity.x >= -100;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        dogeIsGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        dogeIsGrounded = false;
     }
 
     public void gravity()
