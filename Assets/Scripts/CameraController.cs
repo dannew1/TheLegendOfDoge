0f0387006ea2d3dee0a5d103a5e9d53d501ef253
dannew1 <daniel.willforss@skola.ljungby.se>
2016-10-22 18:05:56 +0200

    camera och enemy movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Doge player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        //player = GetComponent<Doge>();
+
+        offset = transform.position - player.transform.position;
+        Debug.Log(player);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/Who_are_we.cs
+++ Assets/Scripts/Who_are_we.cs
@@ -3,18 +3,46 @@ using System.Collections;
 
 public class Who_are_we : MonoBehaviour {
 
+	private Rigidbody2D rigid;
+    private float time;
+    private float goToPosition;
+    public float speed = 0.7F;
 	// Use this for initialization
 	void Start () {
-        
-	}
+        rigid = GetComponent<Rigidbody2D>();
+        time = 2;
+        goToPosition = Random.Range(100, 900);
+        Debug.Log(goToPosition);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        enemy_movement();
+        
+    }
 
-    public void OnTriggerEnter2D(Collider2D collider)
+    public void enemy_movement()
     {
-        Debug.Log("hej");
+
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            if (transform.position.x + 10 >= goToPosition && transform.position.x - 10 <= goToPosition)
+            {
+                rigid.velocity = new Vector2(0, rigid.velocity.y);
+                goToPosition = Random.Range(100, 900);
+                time = Random.Range((float)0.3, 4);
+                Debug.Log(goToPosition);
+            }
+            else if (goToPosition >= transform.position.x)
+            {
+                rigid.velocity += new Vector2(speed, 0);
+            }
+
+            else if (goToPosition <= transform.position.x)
+            {
+                rigid.velocity += new Vector2(-speed, 0);
+            }
+        }
     }
 }
