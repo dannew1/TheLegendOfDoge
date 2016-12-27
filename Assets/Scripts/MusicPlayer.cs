b0d1e26663e6e4fe85f2f3e6ba53989f8256c095
dannew1 <daniel.willforss@skola.ljungby.se>
2016-12-27 11:03:59 +0100

    music and player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private AudioSource Level1;
+
+	// Use this for initialization
+	void Start () {
+
+        Level1 = GetComponent<AudioSource>();
+
+        //if (Level1 != null)
+        Level1.Play();
+
+        //Level1.playOnAwake = true;
+
+        Debug.Log("hej");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
