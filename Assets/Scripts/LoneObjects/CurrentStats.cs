3c2d2383e70f3ab8e4552db7a09b4bfe2cae4052
dannew1 <daniel.willforss@skola.ljungby.se>
2017-07-30 12:04:59 +0200

    % on statBars

---
+++ Assets/Scripts/LoneObjects/CurrentStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CurrentStats : MonoBehaviour {
+
+    public Doge player;
+    public Image hpSprite;
+    public Image spSprite;
+    public Text hpText;
+    public Text spText;
+
+    private Shooting shootingScript;
+    private DogeHealth dogeHealthScript;
+
+    // Use this for initialization
+    void Start () {
+        shootingScript = player.GetComponent<Shooting>();
+        dogeHealthScript = player.GetComponent<DogeHealth>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        hpSprite.color = new Color(1, 1 - player.healthValue / dogeHealthScript.maxHealth, 1 - player.healthValue / dogeHealthScript.maxHealth, 1);
+        spSprite.color = new Color(1 - player.manaValue / shootingScript.maxMana, 1 - player.manaValue / shootingScript.maxMana, 1, 1);
+        hpText.text = Mathf.Round(player.healthValue / dogeHealthScript.maxHealth * 100) + "%";
+        spText.text = Mathf.Round(player.manaValue / shootingScript.maxMana * 100) + "%";
+    }
+
+}
